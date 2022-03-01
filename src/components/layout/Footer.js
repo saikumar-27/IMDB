0a0bf1d7f17214c6d07ed69e2b9e55fba44f7d10
sai kumar <saiyadav27398@gmail.com>
2022-03-01 14:51:03 +0530

    Movie components

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="footer p-3 mt-4 text-center bg-dark text-light">
+            Developed By :
+            <span className="text-warning font-weight-normal">
+              {" "}
+              <a
+                href="https://github.com/saikumar-27"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                S.Sai Kumar Yadav
+              </a>
+            </span>
+            , Using <i className="fab fa-react" /> React JS &amp; Redux JS
+            integrated with external movies data API{" "}
+            <a
+              href="http://www.omdbapi.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              OMDB
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
